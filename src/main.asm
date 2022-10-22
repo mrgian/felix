00975ce065f43a25051c56fadb185b4fcae6af44
mrgian <mail@gian.im>
2022-10-22 13:39:19 +0200

    Add comments

--- src/main.asm
+++ src/main.asm
@@ -1,12 +1,12 @@
-org 0x7C00
+org 0x7C00 ;bios puts the os in ram at 0x7c00, so tell nasm to calculate addresses from this address
 
-bits 16
+bits 16 ;tell nasm to generate 16bit code
 
 main:
-    hlt
+    hlt ;just halt the cpu
 
-.halt
+.halt ;in some cases might execute other istruction after the end, this loop prevents that
     jmp .halt
 
-times 510($-$$) db 0
-dw 0AA55h
+times 510-($-$$) db 0 ;put all zeros till byte 510, so write 0 for 510-(program size)
+dw 0AA55h ;put 0xaa55 signature as two last bytes of program
