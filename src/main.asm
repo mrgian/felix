e181f99023736b2cb1c89c1254d5a0576c05f858
mrgian <mail@gian.im>
2023-01-26 18:47:44 +0100

    Change hex notation again

--- src/main.asm
+++ src/main.asm
@@ -1,4 +1,4 @@
-org 7C00h ;bios puts the os in ram at 0x7c00, so tell nasm to calculate addresses from this address
+org 0x7c00 ;bios puts the os in ram at 0x7c00, so tell nasm to calculate addresses from this address
 
 bits 16 ;tell nasm to generate 16bit code
 
@@ -9,4 +9,4 @@ main:
     jmp .halt
 
 times 510-($-$$) db 0 ;put all zeros till byte 510, so write 0 for 510-(program size)
-dw AA55h ;put 0xaa55 signature as two last bytes of program
+dw 0xaa55 ;put 0xaa55 signature as two last bytes of program
