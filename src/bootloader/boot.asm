0085f874c5e03b3eed1c6bf3ab1f85ea182f38d4
mrgian <mail@gian.im>
2023-01-29 16:41:01 +0100

    Add fat12 header to bootloader

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -7,6 +7,34 @@ bits 16
 ;define endl as line feed + carriage return
 %define ENDL 0x0d, 0x0a
 
+;FAT12 header
+;we need to put this because the bootloader will overwrite the first sector of fat12 formatted image
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+;extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'FELIX      '        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
+
+
 ;there are other functions before main, with this we keep main the entry point
 start:
     jmp main
