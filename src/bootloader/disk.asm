5ed4f043c43f82908df1e31aa96e17f1afce300a
mrgian <mail@gian.im>
2023-01-30 18:57:53 +0100

    Add lba to chs calculation

---
+++ src/bootloader/disk.asm
@@ -0,0 +1,27 @@
+;converts LBA address (Logical Block Addrress) to CHS address (cylinder, head, sector)
+;parameter:
+;   ax -> LBA address
+;returns:
+;   cx (bits 0-5) -> sector
+;   cx (bits 6-15) -> cylinder
+;   dh -> head 
+lba_to_chs:
+    xor dx, dx                          ;dx = 0
+    div word [bdb_sectors_per_track]    ;ax = LBA / sectors_per_track
+                                        ;dx = LBA % sectors_per_track
+    inc dx                              ;dx = (LBA % sectors_per_track) + 1
+    mov cx, dx                          ;cx = sector
+
+    xor dx, dx                          ;dx = 0
+    div word [bdb_heads]                ;ax = (LBA / sectors_per_track) / heads = cylinder
+                                        ;dx = (LBA / sectors_per_track) % heads = head
+    
+    mov dh, dl                          ;dh = head (because heads number is in the lower part od dx)
+    mov ch, al                          ;ch = cylinder (only the lower 8 bits, there are 2 more to put in cl)
+    shl ah, 6
+    or cl, ah                           ;now we put upper 2 bits of cylinder in cl
+
+;Representation of this mess:
+;cx         = [  CH  ] [  CL  ]
+;cylinder   = XXXXXXXX XX
+;sector     =            XXXXXX
