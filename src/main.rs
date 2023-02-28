6100f35df97025d0377400884fccdb22acdd2a32
mrgian <mail@gian.im>
2023-02-28 12:50:37 +0100

    Make print function ffi-safe

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use core::arch::asm;
 
 global_asm!(include_str!("bootloader/boot.asm"));
 
-const msg: &str = "Hello from Rust!\r\n";
+const MSG: &[u8] = b"Hello from Rust!\r\n\0";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -15,15 +15,15 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn print(message: &str) {
+pub extern "C" fn print(message_pointer: *const u8) {
     unsafe{
-        asm!(include_str!("bootloader/print.asm"), in(reg) message.as_ptr());
+        asm!(include_str!("bootloader/print.asm"), in(reg) message_pointer);
     }
 }
 
 #[no_mangle]
 pub extern "C" fn main() {
-    print(msg);
+    print(MSG.as_ptr());
 }
 
 /*#[no_mangle]
