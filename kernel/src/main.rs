212713cea23672b680348daea2a564851a8f5442
mrgian <mail@gian.im>
2023-03-07 23:56:06 +0100

    Implemented fmt::write trait

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,15 +3,14 @@
 
 //use core::arch::asm;
 use core::panic::PanicInfo;
+use core::fmt::Write;
+use core::arch::asm;
 
 mod print;
-use print::printc;
-use print::prints;
 
 //TODO: Handle kernel panic, instead of looping
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-
     //loops but it shouldn't
     //like people when they have a panic attack
     //their mind loops instead of dealing with problems
@@ -21,9 +20,21 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 #[link_section = ".start"]
 pub extern "C" fn _start() -> ! {
-    prints("Loaded!\r\nWelcome to Felix!\r\n\0");
-    printc('X');
-    printc('o');
+    //TODO: make a println macro
+    let mut printer = print::Printer {};
+    write!(printer, "Loaded!\n\rWelcome to Felix\n\r").unwrap();
+    write!(printer, "Hello {}\n\r", "world").unwrap();
+    write!(printer, "{} {}\n\r", 69, 420).unwrap();
+
+    let mut x: u16;
+    unsafe {
+        asm!(
+            "mov {x}, ebx",
+            x = out(reg) x
+        );
+    }
+
+    write!(printer, "Current ebx reg: {:X}", x).unwrap();
 
     loop {}
 }
--- kernel/src/print.rs
+++ kernel/src/print.rs
@@ -1,8 +1,46 @@
 use core::arch::asm;
+use core::fmt;
+
+pub struct Printer {}
+
+impl fmt::Write for Printer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.prints(s);
+        Ok(())
+    }
+}
+
+impl Printer {
+    //print only a char
+    pub fn printc(&self, c: char) {
+        unsafe {
+            asm!(
+            "mov ah, 0x0e",
+            "mov bh, 0",
+            "int 0x10", //tell the bios to write content of al to screen
+            in("ax") c as u16);
+        }
+    }
+
+    //print a string
+    pub fn prints(&self, s: &str) {
+        for c in s.chars() {
+            self.printc(c);
+        }
+    }
+
+    //set bios video mode to clear the screen
+    #[allow(dead_code)]
+    pub fn clear() {
+        unsafe {
+            asm!("mov ah, 0x00", "mov al, 0x03", "int 0x10");
+        }
+    }
+}
 
 //bios interrupt to print to the screen
 //TODO: Implement a printf function that is able to format strings
-pub fn prints(message: &str) {
+/*pub fn print(message: &str) {
     unsafe {
         asm!("mov si, {0:x}", //move given string address to si
             "2:",
@@ -18,23 +56,4 @@ pub fn prints(message: &str) {
             "1:",
             in(reg) message.as_ptr());
     }
-}
-
-//print only a char
-pub fn printc(c: char) {
-    unsafe {
-        asm!(
-            "mov ah, 0x0e",
-            "mov bh, 0",
-            "int 0x10", //tell the bios to write content of al to screen
-            in("ax") c as u16);
-    }
-}
-
-//set bios video mode to clear the screen
-#[allow(dead_code)]
-pub fn clear() {
-    unsafe {
-        asm!("mov ah, 0x00", "mov al, 0x03", "int 0x10");
-    }
-}
+}*/
