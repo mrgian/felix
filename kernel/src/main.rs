e286dc9e36dd92eecc93e676fe4984d2eba2e6c4
mrgian <mail@gian.im>
2023-03-21 18:30:04 +0100

    Start implementing vga text buffer print

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -12,9 +12,45 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 #[link_section = ".start"]
 pub extern "C" fn _start() -> ! {
-    unsafe { 
-        asm!("mov al, 0x58", "mov ah, 0x0f", "mov [0xb8000], ax");
-    }
+    print("Hello from kernel\0");
 
     loop {}
 }
+
+fn print(message: &str) {
+    unsafe {
+        asm!("mov esi, {0:e}",
+            "mov edi, 0x000b8000",
+            "cld",
+            "2:",
+            "lodsb",
+            "or al, al",
+            "jz 1f",
+
+            "mov [edi], al",
+            "inc edi",
+            "inc edi",
+
+            "jmp 2b",
+            "1:",
+            in(reg) message.as_ptr());
+    }
+}
+
+/*pub fn print(message: &str) {
+    unsafe {
+        asm!("mov si, {0:x}", //move given string address to si
+            "2:",
+            "lodsb", //load a byte (next character) from si to al
+            "or al, al", //bitwise or on al, if al is null set zf to true
+            "jz 1f", //if zf is true (end of string) jump to end
+
+            "mov ah, 0x0e",
+            "mov bh, 0",
+            "int 0x10", //tell the bios to write content of al to screen
+
+            "jmp 2b", //start again
+            "1:",
+            in(reg) message.as_ptr());
+    }
+}*/
